072b2781c5aafa4cde6502bdada9f587f3964d82
Deucalio <hjamad89@gmail.com>
2023-11-07 21:54:58 +0500

    add service page template

---
+++ app/[service]/page.jsx
@@ -0,0 +1,98 @@
+
+export default function Service({params}) {
+    const {service} = params
+  return (
+   <>
+   <p>Service: {service}</p>
+   </>
+  )
+}
+
+
+
+{/* <nav className="mx-auto w-[95%] px-2 py-4 text-slate-300 sm:px-12">
+<ul className="mx-auto flex flex-row justify-between gap-5 text-[1rem] uppercase tracking-wider sm:gap-0 sm:text-base">
+  <Link href="/">
+    <li className="w-14 cursor-pointer self-center border-b-[1px] border-slate-100 border-opacity-0 py-1 text-center text-xs transition-all duration-700 ease-in-out hover:border-opacity-95">
+      Back
+    </li>
+  </Link>
+  <li className="w-18 cursor-pointer border-[1px] border-slate-100 px-6 py-2 text-center text-xs font-semibold transition-all duration-700 ease-in-out hover:bg-white hover:text-black">
+    Send us a Message
+  </li>
+</ul>
+</nav>
+
+<section className="relative h-[75vh] md:h-[90vh]">
+<img
+  className="h-full w-full opacity-60"
+  src="https://images.unsplash.com/photo-1633269540827-728aabbb7646?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  alt=""
+/>
+<p className="absolute top-1/3 w-5/6 text-right text-3xl font-semibold uppercase leading-tight tracking-wide text-white opacity-90 sm:right-16 sm:w-1/2 md:right-24 md:top-1/3 md:w-1/2 md:text-4xl xl:w-2/5">
+  Modern Web Development
+</p>
+</section>
+
+<section className="px-8 py-24">
+<ul className="grid grid-cols-1 gap-28 text-white md:grid-cols-2">
+  <li className="flex h-fit flex-col text-center">
+    <img
+      className="h-44 w-44 md:h-56 md:w-56 mx-auto"
+      src="https://i.imgur.com/wSMg524.png"
+      alt=""
+    />
+    <p className="py-3 text-sm xl:text-lg font-semibold leading-snug">
+      Responsive Web Design
+    </p>
+    <p className="mx-auto text-xs opacity-70 tracking-normal sm:w-1/2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+      tenetur earum est velit quaerat perspiciatis.
+    </p>
+  </li>
+
+  <li className="flex h-fit flex-col text-center">
+    <img
+      className="h-44 w-44 md:h-56 md:w-56 mx-auto"
+      src="https://i.imgur.com/NkBwVdD.png"
+      alt=""
+    />
+    <p className="py-3 text-sm xl:text-lg font-semibold leading-snug">
+      Design to Development
+    </p>
+    <p className="mx-auto text-xs opacity-70 tracking-normal sm:w-1/2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+      tenetur earum est velit quaerat perspiciatis.
+    </p>
+  </li>
+  <li className="flex h-fit flex-col text-center">
+    <img
+      className="h-44 w-44 md:h-56 md:w-56 mx-auto"
+      src="https://i.imgur.com/j0YkXFE.png"
+      alt=""
+    />
+    <p className="py-3 text-sm xl:text-lg font-semibold leading-snug">
+      Seamless UI/UX
+    </p>
+    <p className="mx-auto text-xs opacity-70 tracking-normal sm:w-1/2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+      tenetur earum est velit quaerat perspiciatis.
+    </p>
+  </li>
+
+  <li className="flex h-fit flex-col text-center">
+    <img
+      className="h-44 w-44 md:h-56 md:w-56 mx-auto"
+      src="https://i.imgur.com/TWmwnom.png"
+      alt=""
+    />
+    <p className="py-3 text-sm xl:text-lg font-semibold leading-snug">
+      Performance Optimization
+    </p>
+    <p className="mx-auto text-xs opacity-70 tracking-normal sm:w-1/2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+      tenetur earum est velit quaerat perspiciatis.
+    </p>
+  </li>
+</ul>
+</section> */}
