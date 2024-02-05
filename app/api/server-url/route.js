eb70ef663d5323a0aa6d1479a40182119594e825
Deucalio <hjamad89@gmail.com>
2024-02-05 17:59:04 +0500

    test

--- app/api/server-url/route.js
+++ app/api/server-url/route.js
@@ -3,6 +3,10 @@ export const dynamic = "force-dynamic"; // defaults to auto
 import { PrismaClient } from "../../../prisma/generated/client";
 
 export async function GET(request) {
+
+  // if request is from localhost, we will return the server URL as localhost
+  console.log("request", request.headers.get("host"));
+
   return Response.json({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || process.env.SERVER_URL,
   });
