071c22a66da13242f53e098dee00b01f179fa757
Deucalio <hjamad89@gmail.com>
2024-02-10 18:33:38 +0500

    add favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,7 @@
+{
+  /* <link rel="icon" href="/public/favicon.png" sizes="any" />; */
+}
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./components/AuthProvider";
@@ -12,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />;
+      </head>
       <AuthProvider>
         <body className={inter.className}>{children}</body>
       </AuthProvider>
