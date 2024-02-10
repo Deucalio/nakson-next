e38e6d0ea5174de26fa5dc665fdc23b5964f8034
Deucalio <hjamad89@gmail.com>
2024-02-10 18:58:33 +0500

    add favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/app/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/app/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/app/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/app/favicon-16x16.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/app/app/android-chrome-192x192.png"/>
+
       </head>
       <AuthProvider>
         <body className={inter.className}>{children}</body>
