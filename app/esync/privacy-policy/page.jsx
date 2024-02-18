003f193ecbfc0ba5ab5219741400e849e9be9ff0
Deucalio <hjamad89@gmail.com>
2024-02-18 19:59:50 +0500

    add privacy policy page

---
+++ app/esync/privacy-policy/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div id="" className="privacy-policy h-screen bg-black">
+      <p>Privacy Policy</p>
+    </div>
+  );
+};
+
+export default Page;
