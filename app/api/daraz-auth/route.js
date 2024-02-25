226c904bfc846a2deaf32d94057bd8217202082f
Deucalio <hjamad89@gmail.com>
2024-02-25 17:36:11 +0500

    add psot

--- app/api/daraz-auth/route.js
+++ app/api/daraz-auth/route.js
@@ -3,10 +3,16 @@ const axios = require("axios");
 
 export async function GET(request) {
   // Get the query string
-  console.log("request: ",request.url)
-  
+  console.log("request: ", request.url);
 
   return Response.json({
     message: "Hello from the API!",
+  });
+}
+
+export async function POST(request) {
+  console.log("Post Request: ", request.url);
+  return Response.json({
+    message: "That is a POST REQUEST",
   });
 }
