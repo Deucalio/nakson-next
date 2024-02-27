0f2ad2e5f9c8b390bf8d866de9d848f8adb0590e
Deucalio <hjamad89@gmail.com>
2024-02-27 15:18:08 +0500

    add sign

--- app/Setting/darazcallback/route.js
+++ app/Setting/darazcallback/route.js
@@ -11,7 +11,7 @@ function getTimeStamp() {
   return timestampSeconds * 1000;
 }
 
-async function getSignature(code, timeStamp, params) {
+async function getSignature(secret, api, params) {
   const res = await axios.post("https://esync-backend.vercel.app/api/sign", {
     secret: "sad",
     api: "/auth/token/create",
@@ -22,23 +22,29 @@ async function getSignature(code, timeStamp, params) {
 
 export async function GET(request) {
   // Get the query string
-  // const code = request.url.split("?")[1].slice(5);
+  const code = request.url.split("?")[1].slice(5);
   const { name, email } = JSON.parse(cookies().get("userInfo").value);
-  console.log(name, email);
+  console.log(name, email, email);
 
-  // const code = "4_501634_RQi2t4NkGOosxhvfKtWEay9l65";
-  // const timeStamp = getTimeStamp();
-  // const signature = getSignature();
+  const timeStamp = getTimeStamp();
+  const signature = getSignature(
+    "X1BBDAi3EuamELmOZi400PLT1xxhxrOw",
+    "/auth/token/create",
+    {
+      app_key: "501634",
+      code,
+      sign_method: "sha256",
+      timestamp: timeStamp,
+    }
+  );
 
   // const accessTokenUrl = `https://api.daraz.pk/rest/auth/token/create?code=${code}&app_key=501634&sign_method=sha256&timestamp=${timeStamp}&sign=E4C3B3D46B8FB7023D687BDCC7423B0953DE2287E2FD0C6E0A9435B09A325501`;
-
   // const res = await axios.post(accessTokenUrl);
   // console.log("accessTokenUrl: ", accessTokenUrl);
   // console.log("Response: ", res);
 
   return Response.json({
     message: "Hello from the API!",
-    name,
-    email,
+    signature: signature,
   });
 }
