fa4fe801ce36d728d4bc25b1ca861564e1b7b73f
Deucalio <hjamad89@gmail.com>
2024-03-14 17:20:42 +0500

    push to prod

--- app/api/inngest/functions.ts
+++ app/api/inngest/functions.ts
@@ -1,15 +1,75 @@
 import { inngest } from "./client";
 import axios from "axios";
-
-export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
-    async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        const serverRes = await axios.get("https://nakson.services/api/server-url");
-        const { serverURL } = serverRes.data;
-        const res = await axios.get(`${serverURL}/kewl`);
-        console.log("res: ", res.data);
-        return { event, body: res.data };
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+async function createOrder(numberOfOrders) {
+  const data = {
+    order: {
+      financial_status: "pending",
+      line_items: [
+        {
+          title: "Green Gummy Gums",
+          price: 750.99,
+          grams: "1300",
+          quantity: 3,
+          tax_lines: [
+            {
+              price: 73.5,
+              rate: 0.06,
+              title: "State tax",
+            },
+          ],
+        },
+      ],
+      total_tax: 13.5,
+      currency: "EUR",
     },
+  };
+
+  const config = {
+    headers: {
+      "X-Shopify-Access-Token": "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",
+      "Content-Type": "application/json",
+    },
+  };
+  for (let i = 1; i <= numberOfOrders; i++) {
+    try {
+      const response = await axios.post(
+        "https://quickstart-65d173cf.myshopify.com/admin/api/2024-01/orders.json",
+        data,
+        config
+      );
+      console.log(`Order Created: ${i} `, response.data.order.name);
+      // console.log("Hello")
+      if (i % 4 === 0) {
+        if (i === numberOfOrders) {
+          return;
+        }
+        console.log("Waiting for a minute");
+        // Wait for a minute before sending the next request
+        await sleep(60000);
+      }
+    } catch (error) {
+      console.error(`Request ${i + 1} failed:`, error);
+    }
+  }
+}
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    const { serverURL } = event.data;
+    console.log("serverURL: ", serverURL);
+
+    // create 8 orders of shopify
+    createOrder(8).then((res) => {
+      console.log("Orders created");
+    });
+
+    // const res = await axios.get(`${serverURL}/kewl`);
+    // console.log("res: ", res.data);
+    return { event, body: "Sad" };
+  }
 );
--- app/api/shopify/fulfillorders/route.js
+++ app/api/shopify/fulfillorders/route.js
@@ -6,16 +6,32 @@ export const dynamic = "force-dynamic";
 
 // Create a simple async Next.js API route handler
 export async function GET(request) {
-  const serverURL = request.url.includes("localhost")
-    ? "http://localhost:3000"
-    : "https://nakson.services";
+  // const serverURL = request.url.includes("localhost")
+  //   ? "http://localhost:4000"
+  //   : "https://nakson.services";\
+
+  //   {
+  //     "id": "5753533137188",
+  //     "name": "#MD4292",
+  //     "domain": "momdaughts.myshopify.com",
+  //     "access_token": "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",
+  //     "trackingNo": "HD750135756"
+  // }
+
+  // const { ordersData } = await request.json();
+  // console.log("ordersData: ", ordersData);
+  // return NextResponse.json({ name: "Hello Inngest from Next!", ordersData });
+
   // Send your event payload to Inngest
   const res = await inngest.send({
     name: "test/hello.world",
     data: {
-      email: "testFromNext@example.com",
+      s: "sad",
     },
   });
   console.log("res: ", res);
-  return NextResponse.json({ name: "Hello Inngest from Next!", serverURL });
+  return NextResponse.json({
+    name: "Hello Inngest from Next!",
+    ordersData: "sad",
+  });
 }
